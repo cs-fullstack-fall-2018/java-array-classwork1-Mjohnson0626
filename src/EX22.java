92a1f8311f7632e5e64ed512d907f9e96e99d073
Mike <m.g.johnson0626@gmail.com>
2018-07-24 13:39:38 -0500

    check

---
+++ src/EX22.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class EX22 {
+
+    public static void main(String[] args) {
+        ArrayList<String>squad =  new ArrayList<String>();
+        squad.add("Mike");
+        squad.add("De");
+        squad.add("T");
+        squad.add("Jayd");
+        System.out.println(squad);
+
+
+    }
+}
